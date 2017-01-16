b8eb0cab6121d6e1236246909e6babffe97b7dc6
Yulong Tan <yulongtan67@gmail.com>
2017-01-15 16:21:49 -0800

    log other events

--- app/src/main/java/edu/washington/swifties/activityspy/MainActivity.java
+++ app/src/main/java/edu/washington/swifties/activityspy/MainActivity.java
@@ -17,31 +17,37 @@ public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onStart() {
+    super.onStart();
     Log.i(TAG, "onStart event fired");
   }
 
   @Override
   protected void onResume() {
+    super.onResume();
     Log.i(TAG, "onResume event fired");
   }
 
   @Override
   protected void onPause() {
+    super.onPause();
     Log.i(TAG, "onPause event fired");
   }
 
   @Override
   protected void onStop() {
+    super.onStop();
     Log.i(TAG, "onStop event fired");
   }
 
   @Override
   protected void onRestart() {
+    super.onRestart();
     Log.i(TAG, "onRestart event fired");
   }
 
   @Override
   protected void onDestroy() {
+    super.onDestroy();
     Log.e(TAG, "We're going down, Captain!");
   }
 }
